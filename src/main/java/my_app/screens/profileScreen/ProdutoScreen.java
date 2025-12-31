db638050aee0fe776149b619ce61dc50ad59a3ec
eliezer-software-enginner <eliezerassuncaocustodio@gmail.com>
2025-12-31 17:50:45 -0300

    criando tela de cadastro de produtos

--- src/main/java/my_app/screens/profileScreen/ProdutoScreen.java
+++ src/main/java/my_app/screens/profileScreen/ProdutoScreen.java
@@ -13,36 +13,55 @@ public class ProdutoScreen {
 
     public Component render (){
         return new Column(new ColumnProps().paddingAll(15))
-                .child(Container())
+                .child(
+                        new Row(new RowProps().paddingAll(20)
+                                .spacingOf(20), new RowStyler().borderWidth(1).borderColor("black").borderRadius(1))
+                                .child(ContainerLeft())
+                                .child(ContainerRight())
+                )
+                .child(new Text("Informaçoẽs", new TextProps().fontSize(30)))
+                .child(new Text("Dica 1: Pressione CTRL + G para gerar o código de barras"))
+                .child(new Text("Dica 2: O sistema não permite gravar produtos diferentes com o mesmo código de barras!"))
                 ;
     }
 
-    public Component Container (){
-        return new Column(new ColumnProps().paddingAll(10), new ColumnStyler().borderWidth(1).borderColor("black").borderRadius(1))
-                .child(new Row()
+
+    Component ContainerRight(){
+
+        State<String> imagemState = new State<>("/assets/produto-generico.png");
+
+        return new Column()
+                .child(new Text("Foto do produto",new TextProps().fontSize(20).bold()))
+                .child(new Image(imagemState, new ImageProps().size(200)))
+                .child(new Button("Inserir imagem", new ButtonProps().fontSize(20)));
+    }
+     Component ContainerLeft (){
+        var rowProps = new RowProps().spacingOf(10);
+        return new Column(new ColumnProps().spacingOf(20))
+                .child(new Row(rowProps)
                         .child(InputColumn("SKU(Código de barras)", codigoBarrasState))
                         .child(InputColumn("Descrição", descState))
-                ).child(new Row()
+                ).child(new Row(rowProps)
                         .child(InputColumn("Unidade", codigoBarrasState))
                         .child(InputColumn("Preço de compra", descState))
                     .child(InputColumn("Margem", codigoBarrasState))
                     .child(InputColumn("Lucro", descState))
                         .child(InputColumn("Preço de venda", descState))
                 )
-                .child(new Row()
+                .child(new Row(rowProps)
                         .child(InputColumn("Categoria", codigoBarrasState))
                         .child(InputColumn("Fornecedor", descState))//fornecedor padrão
                         .child(InputColumn("Margem", codigoBarrasState))
                         .child(InputColumn("Lucro", descState))
                         .child(InputColumn("Preço de venda", descState))
                 )
-                .child(new Row()
+                .child(new Row(rowProps)
                         .child(InputColumn("Garantia", codigoBarrasState))
                         .child(InputColumn("Marca", descState))//fornecedor padrão
                         .child(InputColumn("Validade", codigoBarrasState))
                         .child(InputColumn("Comissão", descState))
                 )
-                .child(new Row()
+                .child(new Row(rowProps)
                         .child(InputColumn("Observações", codigoBarrasState))
                         .child(InputColumn("Estoque", descState))//fornecedor padrão
                 )
@@ -52,8 +71,8 @@ public class ProdutoScreen {
 
     Component InputColumn(String label, State<String> inputState){
         return new Column()
-                .child(new Text(label))
-                .child(new Input(inputState))
+                .child(new Text(label, new TextProps().fontSize(25)))
+                .child(new Input(inputState,new InputProps().fontSize(20)))
                 ;
     }
 }
